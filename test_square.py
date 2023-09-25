c2776e98fbb48759fe565333369a7de6d946ec32
NetAcad DEVASC <changeme@example.com>
2023-09-25 05:00:03 +0000

    Python Test

--- test_square.py
+++ test_square.py
@@ -2,8 +2,12 @@ import math
 
 def test_sqrt():
     num=25
-    assert math.sqrt(num) == 3
+    assert math.sqrt(num) == 5
 
 def test_square():
     num=7
-    assert 7*7 == 40
+    assert (num*num) == 49
+
+def test_greater():
+    x = 100
+    assert x >= 100
